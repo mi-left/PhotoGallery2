71b5febd75b9020d3fde582002e3b9faff39a6c8
mi-left <91920248+mi-left@users.noreply.github.com>
2022-11-14 20:00:02 -0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery2/api/PhotoInterceptor.kt
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.photogallery2.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+
+private const val API_KEY = "115919e7a73397b2a18419b98bbecf8a"
+class PhotoInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+        val newUrl: HttpUrl = originalRequest.url.newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format", "json")
+            .addQueryParameter("nojsoncallback", "1")
+            .addQueryParameter("extras", "url_s")
+            .addQueryParameter("safesearch", "1")
+            .build()
+        val newRequest: Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
